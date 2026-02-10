9f6cff80dcc89110590171bdae85fc4139e35e2d
Salim Jamal <jamalsalim.js12@gmail.com>
2026-02-10 12:47:34 +0000

    Tailwind CSS config done

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
+
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Hello World</h1>
+      <h1 className="text-3xl font-bold text-red-500">Hello World</h1>
     </div>
   );
 };
