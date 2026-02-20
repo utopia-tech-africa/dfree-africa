58ecab8b5bda3d4fd022ec82d26db1fa22b48da5
Linda Mensah <lindaakmensah@gmail.com>
2026-02-20 14:50:00 +0000

    feat: add our story section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -8,30 +8,32 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="grid h-screen place-items-center \">
-      <div className="relative flex w-full max-w-md flex-col items-center gap-6 rounded-lg bg-amber-50 p-8 shadow-lg">
-        <div className="absolute inset-0 rounded-lg bg-linear-to-t from-black via-transparent to-transparent z-0" />
+    <>
+      <div className="grid h-screen place-items-center \">
+        <div className="relative flex w-full max-w-md flex-col items-center gap-6 rounded-lg bg-amber-50 p-8 shadow-lg">
+          <div className="absolute inset-0 rounded-lg bg-linear-to-t from-black via-transparent to-transparent z-0" />
 
-        <div className="relative z-10 flex flex-col items-center gap-6">
-          <PageTitle
-            text="Sample Text For Page Titles and More"
-            className="text-center"
-          />
-          <Title text="Welcome to the Home Page" />
-          <Subtitle text="This is a subtitle" />
-          <Button
-            variant="default"
-            size="lg"
-            className="bg-purple-400"
-            icon={<ArrowBigRight />}
-          >
-            Click Me
-          </Button>
+          <div className="relative z-10 flex flex-col items-center gap-6">
+            <PageTitle
+              text="Sample Text For Page Titles and More"
+              className="text-center"
+            />
+            <Title text="Welcome to the Home Page" />
+            <Subtitle text="This is a subtitle" />
+            <Button
+              variant="default"
+              size="lg"
+              className="bg-purple-400"
+              icon={<ArrowBigRight />}
+            >
+              Click Me
+            </Button>
+          </div>
         </div>
       </div>
 
       <Footer />
-    </div>
+    </>
   );
 };
 
