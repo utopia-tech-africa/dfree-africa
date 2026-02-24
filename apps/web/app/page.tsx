ad80fc22ef98f380cebe7a6f05c9a8e3ddf68d43
Linda Mensah <lindaakmensah@gmail.com>
2026-02-24 09:13:55 +0000

    style: remove events and blogs from home

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { Events } from "./components/events/events";
-import { Blogs } from "./components/blogs";
 
 const Home = () => {
   return (
     <>
-      <Events />
-      <Blogs />
+      <p>HOME</p>
     </>
   );
 };
