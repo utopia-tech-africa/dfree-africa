81aa58053894d184bf6b0ef4935aeb7406caa5d3
efbrace <ebenezerflintwoodbrace@gmail.com>
2026-03-06 15:21:57 +0000

    feat(web): fix footer

--- apps/web/components/footer/footer.tsx
+++ apps/web/components/footer/footer.tsx
@@ -8,109 +8,111 @@ const { footerAbout, footerPillars, getInvolved, connectWithUs } = footerItems;
 
 export const Footer = () => {
   return (
-    <ComponentLayout
-      id="footer"
-      className="mt-20 lg:px-12 w-full scroll-mt-24 bg-primary-500 py-10 text-neutral-100 md:scroll-mt-28 lg:py-12"
-    >
-      <div className="grid gap-8 lg:grid-cols-[auto_1fr_1fr_1fr_1fr] min-w-0">
-        <div className="h-7.25 md:h-11.25 w-fit object-cover">
-          <DfreeLogoWhite />
-        </div>
-        <div className="flex flex-col gap-5">
-          <h4 className="text-lg font-bold">About</h4>
-          <div className="flex flex-col gap-4 text-sm lg:text-base">
-            {footerAbout.map((item, i) => (
-              <a href={""} key={i}>
-                {item}
-              </a>
-            ))}
+    <div className="bg-primary-500">
+      <ComponentLayout
+        id="footer"
+        className="mt-20 lg:px-12 w-full scroll-mt-24  py-10 text-neutral-100 md:scroll-mt-28 lg:py-12"
+      >
+        <div className="grid gap-8 lg:grid-cols-[auto_1fr_1fr_1fr_1fr] min-w-0">
+          <div className="h-7.25 md:h-11.25 w-fit object-cover">
+            <DfreeLogoWhite />
           </div>
-        </div>
-        <div className="flex flex-col gap-5">
-          <h4 className="text-lg font-bold">Our pillars</h4>
-          <div className="flex flex-col gap-4 text-sm lg:text-base">
-            {footerPillars.map((item, i) => (
-              <a href="" key={i} className="text-nowrap">
-                {item}
-              </a>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-col gap-5">
-          <h4 className="text-lg font-bold">Get involved</h4>
-          <div className="flex flex-col gap-4 text-sm lg:text-base">
-            {getInvolved.map(
-              (item, i) =>
-                item && (
-                  <a href="" key={i}>
-                    {item}
-                  </a>
-                ),
-            )}
-          </div>
-        </div>
-        <div className="flex flex-col gap-10">
           <div className="flex flex-col gap-5">
-            <h4 className="text-lg font-bold">Connect with us</h4>
-            {connectWithUs.map((connect, idx) => (
-              <div key={idx} className="flex items-center gap-3">
-                {connect.icons?.map((Icon) => (
-                  <a href="" key={Icon.displayName || Icon.name}>
-                    <Icon size={20} />
-                  </a>
-                ))}
-                {connect.text && (
-                  <span className="text-sm lg:text-base">{connect.text}</span>
-                )}
-              </div>
-            ))}
-          </div>
-          <div className="flex flex-col gap-4">
-            <h4 className="text-lg hidden md:block font-bold"></h4>
-            <h4 className="text-lg font-bold font-montserrat">
-              Subscribe to newsletter
-            </h4>
-            <p className="block md:hidden font-poppins text-sm leading-[120%] text-neutral-100">
-              Stay connected with our latest updates and transformative work
-            </p>
-            <div className="flex w-full flex-col gap-3 md:flex-row md:items-center">
-              <Input
-                type="email"
-                placeholder="Enter email address"
-                className="w-full rounded-[100px] border border-neutral-100 bg-transparent px-2 py-6 placeholder:text-sm placeholder:text-neutral-300 focus:outline-none md:min-w-75 md:flex-1 md:px-5 lg:min-w-62.5"
-              />
-              <Button
-                className="w-full py-6 font-bold md:w-auto"
-                variant={"secondary"}
-              >
-                Submit
-              </Button>
+            <h4 className="text-lg font-bold">About</h4>
+            <div className="flex flex-col gap-4 text-sm lg:text-base">
+              {footerAbout.map((item, i) => (
+                <a href={""} key={i}>
+                  {item}
+                </a>
+              ))}
             </div>
-            <p className="text-xs text-neutral-200">
-              By subscribing, you agree to our privacy policy
-            </p>
+          </div>
+          <div className="flex flex-col gap-5">
+            <h4 className="text-lg font-bold">Our pillars</h4>
+            <div className="flex flex-col gap-4 text-sm lg:text-base">
+              {footerPillars.map((item, i) => (
+                <a href="" key={i} className="text-nowrap">
+                  {item}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col gap-5">
+            <h4 className="text-lg font-bold">Get involved</h4>
+            <div className="flex flex-col gap-4 text-sm lg:text-base">
+              {getInvolved.map(
+                (item, i) =>
+                  item && (
+                    <a href="" key={i}>
+                      {item}
+                    </a>
+                  ),
+              )}
+            </div>
+          </div>
+          <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-5">
+              <h4 className="text-lg font-bold">Connect with us</h4>
+              {connectWithUs.map((connect, idx) => (
+                <div key={idx} className="flex items-center gap-3">
+                  {connect.icons?.map((Icon) => (
+                    <a href="" key={Icon.displayName || Icon.name}>
+                      <Icon size={20} />
+                    </a>
+                  ))}
+                  {connect.text && (
+                    <span className="text-sm lg:text-base">{connect.text}</span>
+                  )}
+                </div>
+              ))}
+            </div>
+            <div className="flex flex-col gap-4">
+              <h4 className="text-lg hidden md:block font-bold"></h4>
+              <h4 className="text-lg font-bold font-montserrat">
+                Subscribe to newsletter
+              </h4>
+              <p className="block md:hidden font-poppins text-sm leading-[120%] text-neutral-100">
+                Stay connected with our latest updates and transformative work
+              </p>
+              <div className="flex w-full flex-col gap-3 md:flex-row md:items-center">
+                <Input
+                  type="email"
+                  placeholder="Enter email address"
+                  className="w-full rounded-[100px] border border-neutral-100 bg-transparent px-2 py-6 placeholder:text-sm placeholder:text-neutral-300 focus:outline-none md:min-w-75 md:flex-1 md:px-5 lg:min-w-62.5"
+                />
+                <Button
+                  className="w-full py-6 font-bold md:w-auto"
+                  variant={"secondary"}
+                >
+                  Submit
+                </Button>
+              </div>
+              <p className="text-xs text-neutral-200">
+                By subscribing, you agree to our privacy policy
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="mt-8">
-        <hr className="border-white" />
-      </div>
-      <div className="mt-6 flex gap-5 flex-col md:flex-row md:items-center justify-center text-sm">
-        <span className="text-center">
-          © 2024 DFREE® Foundation. All rights reserved.
-        </span>
-        <div className="flex gap-3 items-center justify-center md:gap-8 underline text-xs">
-          <a href="" className="cursor-pointer text-nowrap">
-            Privacy policy
-          </a>
-          <a href="" className="cursor-pointer text-nowrap">
-            Terms of service
-          </a>
-          <a href="" className="cursor-pointer text-nowrap">
-            Cookies settings
-          </a>
+        <div className="mt-8">
+          <hr className="border-white" />
         </div>
-      </div>
-    </ComponentLayout>
+        <div className="mt-6 flex gap-5 flex-col md:flex-row md:items-center justify-center text-sm">
+          <span className="text-center">
+            © 2024 DFREE® Foundation. All rights reserved.
+          </span>
+          <div className="flex gap-3 items-center justify-center md:gap-8 underline text-xs">
+            <a href="" className="cursor-pointer text-nowrap">
+              Privacy policy
+            </a>
+            <a href="" className="cursor-pointer text-nowrap">
+              Terms of service
+            </a>
+            <a href="" className="cursor-pointer text-nowrap">
+              Cookies settings
+            </a>
+          </div>
+        </div>
+      </ComponentLayout>
+    </div>
   );
 };
