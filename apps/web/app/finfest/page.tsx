7f0661e38f136386fd74ddc98b91d9dee92904db
Linda Mensah <lindaakmensah@gmail.com>
2026-03-06 15:07:10 +0000

    feat: implement objective section

--- apps/web/app/finfest/page.tsx
+++ apps/web/app/finfest/page.tsx
@@ -1,10 +1,14 @@
 import { FinFestBanner } from "./components";
 import { FinfestHero } from "./components/finfest-hero";
+import { FinfestObjective } from "./components/finfest-objective";
+import FinfestMovement from "./components/finfest-objective/finfest-movement";
 
 export default function FinFestPage() {
   return (
     <div>
       <FinfestHero />
+      <FinfestObjective />
+      <FinfestMovement />
       <FinFestBanner />
     </div>
   );
