1bdf5d81136ebdf38e73b61b0b7cb21f55646e8c
Salim Jamal <jamalsalim.js12@gmail.com>
2026-03-12 06:34:32 +0000

    fix: remove africa banner import

--- apps/web/components/banners/africa-banner.tsx
+++ apps/web/components/banners/africa-banner.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { Banner } from "./banner";
-import { AfricaBannerBg } from "@/assets/img";
 import { useTranslations } from "next-intl";
 
 export const AfricaBanner = ({ classname }: { classname?: string }) => {
